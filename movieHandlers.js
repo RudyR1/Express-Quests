0be4f9aff8d95b015743a2517509d09e4ac03a46
RudyR1 <rudy.rano972@gmail.com>
2023-05-08 16:34:33 +0200

    Express05_DELETE

--- movieHandlers.js
+++ movieHandlers.js
@@ -166,6 +166,44 @@ database
 });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  database
+  .query(
+    "DELETE FROM movies WHERE id = ? ",[id]
+  )
+  .then((result) => {
+    if (result.affectedRows === 0){
+      res.status(404).send("Not Found");
+    }else{
+      res.sendStatus(200)
+    }
+  })
+  .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+  });
+};
+
+const deleteUser = (req, res) => {
+const id = parseInt(req.params.id)
+
+  database
+  .query("DELETE FROM users WHERE id = ? ",[id])
+  .then((result) => {
+    if (result.affectedRows === 0){
+      res.status(404).send("Not Found");
+    }else{
+      res.sendStatus(200)
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error deleting the user");
+  });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -175,4 +213,6 @@ module.exports = {
   postUser,
   updateMovie,
   updateUser,
+  deleteMovie,
+  deleteUser
 };
