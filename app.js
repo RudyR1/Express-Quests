68dd3220830f4f34e95fba02e6612fc6a499d049
RudyR1 <rudy.rano972@gmail.com>
2023-05-08 14:01:12 +0200

    Express04_PUT

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.get("/api/users", movieHandlers.getUsers);
 app.get("/api/users/:id", movieHandlers.getUserById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", movieHandlers.postUser);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/users/:id", movieHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
